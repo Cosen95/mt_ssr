cfae228a28fc0b68fb873668ac11ae62af8597c4
fengshuan <1263215592@qq.com>
2019-01-01 22:42:15 +0800

    feat:footer组件实现（首页页面布局done）

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,19 @@
     <el-main>
       <nuxt/>
     </el-main>
-    <el-footer>Footer</el-footer>
+    <el-footer height="100%">
+      <my-footer/>
+    </el-footer>
   </el-container>
 </template>
 
 <script>
 import MyHeader from '@/components/public/header/index.vue'
+import MyFooter from "@/components/public/footer/index.vue"
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 }
 </script>
