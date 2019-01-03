d0c8907300ff66ae58ed0fb3dcc9e066638361f0
fengshuan <1263215592@qq.com>
2019-01-03 22:39:48 +0800

    feat:登录页面done

---
+++ pages/login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="page-login">
+    <div class="login-header">
+      <a 
+        href="/" 
+        class="logo" />
+    </div>
+    <div class="login-panel">
+      <div class="banner">
+        <img 
+          src="//s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg" 
+          alt="美团网"
+          width="480"
+          height="370"
+        >
+      </div>
+      <div class="form">
+        <h4
+          v-if="error"
+          class="tips"
+        ><i/>{{ error }}</h4>
+        <p><span>账号登录</span></p>
+        <el-input
+          v-model="password"
+          prefix-icon="password"
+          type="password"
+        />
+        <div class="foot">
+          <el-checkbox v-model="checked">7天内自动登录</el-checkbox>
+          <b>忘记密码？</b>
+        </div>
+        <el-button
+          class="btn-login"
+          type="success"
+          size="mini"
+          @click="login"
+        >登录</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            checked: '',
+            username: '',
+            password: '',
+            error: ''
+        }
+    },
+    layout: 'blank'
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/css/login/index.scss"
+</style>
