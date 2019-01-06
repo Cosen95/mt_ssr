ece44ffa1f85d592bc4f51d58f110fa564916314
fengshuan <1263215592@qq.com>
2019-01-06 17:14:11 +0800

    feat:登录到首页&&注销功能实现

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -22,7 +22,13 @@ export default {
         return {
             user: ''
         }
-    }
+    },
+    async mounted() {
+      const { status, data: { user }} = await this.$axios.get('/users/getUser')
+      if(status === 200) {
+       this.user = user 
+      }
+    },
 }
 </script>
 
