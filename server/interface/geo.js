b87839a4e30a48ca4448f7d8163143e179f261fc
fengshuan <1263215592@qq.com>
2019-01-24 22:23:10 +0800

    feat:geo相关接口编写

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -2,6 +2,7 @@ import Router from 'koa-router'
 import axios from './utils/axios'
 import Province from '../dbs/models/province'
 import Menu from '../dbs/models/menu'
+import City from '../dbs/models/city'
 
 let router = new Router({
     prefix: '/geo'
@@ -42,6 +43,61 @@ router.get('/province', async (ctx) => {
                 name: item.value[0]
             }
         })
+    }
+})
+
+router.get('/province/:id',async (ctx) => {
+    let city = await City.findOne({id: ctx.params.id})
+    ctx.body = {
+        code: 0,
+        city: city.value.map(item => {
+            return {
+                province: item.province,
+                id: item.id,
+                name: item.name
+            }
+        })
+    }
+})
+
+router.get('/city', async (ctx) => {
+    let city = []
+    let result = await City.find()
+    result.forEach(item => {
+        city = city.concat(item.value)
+    })
+    ctx.body = {
+        code: 0,
+        city: city.map(item => {
+            return {
+                province: item.province,
+                id: item.id,
+                name: item.name === '市辖区' || item.name === '省直辖县级行政区划' ? item.province : item.name
+            }
+        })
+    }
+})
+
+router.get('/hotCity', async (ctx) => {
+    let list = [
+    '北京市',
+    '上海市',
+    '广州市',
+    '深圳市',
+    '天津市',
+    '西安市',
+    '杭州市',
+    '南京市',
+    '武汉市',
+    '成都市'
+    ]
+    let result = await City.find()
+    let nList = []
+    result.forEach(item => {
+        nList = nList.concat(item.value.filter(k => list.includes(k.name) || list.includes(k.province)))
+    })
+    ctx.body = {
+        hots: nList
     }
 })
 
