56719776f2ec014030f1872d14337a8d0aba5eaf
fengshuan <1263215592@qq.com>
2019-02-01 22:26:03 +0800

    feat:城市选择页面init

---
+++ components/changeCity/iselect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="m-iselect">
+    <span class="name">按省份选择：</span>
+    <el-select
+      v-model="pvalue"
+      placeholder="省份">
+      <el-option
+        v-for="item in province"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"/>   
+    </el-select>
+    <el-select
+      v-model="cvalue"
+      :disabled="!city.length"
+      placeholder="城市">
+      <el-option
+        v-for="item in city"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value" />
+    </el-select>
+    <el-autocomplete
+      v-model="input"
+      placeholder="请输入城市中文或拼音" />
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+    data() {
+        return {
+            province: [],
+            pvalue: '',
+            city: [],
+            cvalue: '',
+            input: '',
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/css/changeCity/iselect.scss"
+</style>
+
+
---
+++ pages/changeCity.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24">
+        <iSelect />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"/>
+    </el-row>
+    <el-row>
+      <el-col :span="24"/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import iSelect from '@/components/changeCity/iselect.vue'
+export default {
+    components: {
+        iSelect
+    }
+}
+</script>
+
+<style lang="css">
+
+</style>
+
