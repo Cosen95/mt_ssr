1953fdc0fed2b51f7e83bbdefa279b2c5588469c
fengshuan <1263215592@qq.com>
2019-02-16 16:36:28 +0800

    feat:按拼音首字母选择(js-pinyin)

--- pages/changeCity.vue
+++ pages/changeCity.vue
@@ -11,7 +11,9 @@
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24"/>
+      <el-col :span="24">
+        <categroy/>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -19,10 +21,12 @@
 <script>
 import iSelect from '@/components/changeCity/iselect.vue'
 import Hot from '@/components/changeCity/hot.vue'
+import Categroy from '@/components/changeCity/categroy.vue'
 export default {
     components: {
         iSelect,
-        Hot
+        Hot,
+        Categroy
     }
 }
 </script>
