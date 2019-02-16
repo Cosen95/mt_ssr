895cffe6094ece0e084b4ca250f88df629970637
fengshuan <1263215592@qq.com>
2019-02-16 15:36:22 +0800

    feat:热门城市

--- pages/changeCity.vue
+++ pages/changeCity.vue
@@ -6,7 +6,9 @@
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24"/>
+      <el-col :span="24">
+        <hot/>
+      </el-col>
     </el-row>
     <el-row>
       <el-col :span="24"/>
@@ -16,9 +18,11 @@
 
 <script>
 import iSelect from '@/components/changeCity/iselect.vue'
+import Hot from '@/components/changeCity/hot.vue'
 export default {
     components: {
-        iSelect
+        iSelect,
+        Hot
     }
 }
 </script>
